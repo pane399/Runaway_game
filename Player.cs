f60aa4b0d4bc8b1cd5b1503442847adec548ddf3
Yun JH <pane399@korea.ac.kr>
2021-01-19 00:40:21 +0900

    21.01.19 Player Jump & README Edit

---
+++ Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int jumpPower;
+    public bool isJumping;
+
+    Rigidbody2D rigid;
+
+    void Awake() 
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() 
+    {
+        //Jump
+        if(Input.GetButtonDown("Jump") && !isJumping){
+            rigid.AddForce(Vector2.up *jumpPower, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    void FixedUpdate() {
+        
+        if(rigid.velocity.y < 0){
+            Debug.DrawRay(rigid.position, Vector3.down * 2, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down * 2, 1, LayerMask.GetMask("Platform"));
+
+            if(rayHit.collider != null){
+                if(rayHit.distance < 1){
+                    Debug.Log(rayHit.collider.name);
+                    isJumping = false;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Runaway_game
-2D platformer game
+## 2D platformer game
+
+### Features
+- Players can use skills depend on which weapons they have
+- Usable Buttons are only two : Jump, Attack
+- Enemies will appear from right side. Player should take down all of them.
