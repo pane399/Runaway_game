6ac8a24f9b989284b455a10187f421ff1c88941d
Yun JH <pane399@korea.ac.kr>
2021-02-03 19:22:58 +0900

    21.02.03 Obstacle on/off edit, README file update

--- Obstacle.cs
+++ Obstacle.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public int moveSpeed;
+    public float moveSpeed;
 
     Rigidbody2D rigid;
+    BoxCollider2D boxCol;
 
     void Awake() 
     {
         rigid = GetComponent<Rigidbody2D>();
+        boxCol = GetComponent<BoxCollider2D>();
+    }
 
-        rigid.velocity = Vector2.left * moveSpeed;
+    void Update() 
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+
+        offCollider();
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.tag == "Border"){
             Debug.Log(collision.gameObject.tag);
             Destroy(gameObject);
+        }
+    }
+
+    void offCollider(){
+        Player player = GameObject.Find("Player").GetComponent<Player>();
+        Rigidbody2D playerRigid = player.GetComponent<Rigidbody2D>();
+
+        if (player.isJumping && playerRigid.velocity.y > 0){
+            boxCol.enabled = false;
+        }
+        else {
+            boxCol.enabled = true;
         }
     }
 }
--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 ## 21.01.27
 Made Obstacle's moving system. It's speed must be stablized, Even if player step on it.
 Now we need to make pulling objects to manage obstacles.
+
+## 21.02.03
+Edited Obstacle's moving system and collider system. When player jumps, collider would be diabled and would be enabled again if player lands on the obstacle. <br>
+Now I have to make spawning point and manager for obstacles.
